0c719eba0462b31ac0d8eb83eb96131f5fc41843
Kalyan Madhunala <madhunalakalyan55@gmail.com>
2026-04-08 17:54:21 +0530

    UPDATED BACKEND

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -2,9 +2,6 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
   auth: {
     user: process.env.USER_EMAIL,
     pass: process.env.APP_PASSWORD,
